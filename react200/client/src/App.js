cbab85032e8767fe2202abc439a7b880e1f73699
ehs2803 <hr2803@naver.com>
2022-05-07 22:57:59 +0900

    reactstart

---
+++ react200/client/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+	<h1>Start React 200!</h1>
+	<p>HTML 적용하기</p>
+    </div>
+  );
+}
+
+export default App;
