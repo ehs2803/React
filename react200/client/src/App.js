48a5ab8fe25bbf0150035811ab5e6585162d0a15
ehs2803 <hr2803@naver.com>
2022-05-09 08:16:56 +0900

    20220509

--- react200/client/src/App.js
+++ react200/client/src/App.js
@@ -6,6 +6,18 @@ import LifecycleEx from "./R007_LifecycleEx"
 import Es6 from "./R009_Es6"
 import Variable from './R010_Variable'
 import SpreadOperator from './R011_SpreadOperator'
+import ClassPrototype from './R012_Class&Prototype'
+import ArrowFunc from './R013_ArrowFunction'
+import ForEach from './R014_ForEach'
+import Map from './R015_Map'
+import Jquery from './R016_Jquery'
+import Props from './R017_Props'
+import PropsDatatype from './R018_PropsDatatype'
+import PropsBoolean from './R019_PropsBoolean'
+import PropsObjVal from './R020_PropsObjVal'
+import PropsRequired from './R021_PropsRequired'
+import PropsDefault from './R022_PropsDefault'
+import PropsNode from './R023_PropsNode'
 
 function App() {
   return (
@@ -19,6 +31,27 @@ function App() {
         <Es6/>
         <Variable/>
         <SpreadOperator/>
+        <ClassPrototype/>
+        <ForEach/>
+        <Map/>
+        <Jquery/>
+        <Props props_val="THIS IS PROPS"/>
+        <PropsDatatype
+            String="react"
+            Number={200}
+            Boolean={1==1}
+            Array={[0, 1, 8]}
+            ObjectJson={{react:"리액트", twohundred:"200"}}
+            Function={console.log("FunctionProps: function!")}
+        />
+        <PropsBoolean BooleanTrueFalse={false}/>
+        <PropsBoolean BooleanTrueFalse/>
+        <PropsObjVal ObjectJson={{react:"리액트", twohundred:"200"}}/>
+        <PropsDefault ReactNumber={200}/>
+        <PropsNode>
+            <span>node from App.js</span>
+        </PropsNode>
+
     </div>
   );
 }
---
+++ react200/client/src/R014_ForEach.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+
+class R014_ForEach extends Component {
+
+  componentDidMount() {
+    var Es5_Arr = [ 3, 2, 8, 8 ]
+    var Es5_newArr = []
+    
+    for (var i = 0; i < Es5_Arr.length; i++) {
+      Es5_newArr.push(Es5_Arr[i])
+    }
+    console.log("1. Es5_newArr : ["+Es5_newArr+"]")
+
+    var Es6_Arr = [ 3, 3, 9, 8 ]
+    var Es6_newArr = []
+    Es6_Arr.forEach((result) => {
+      Es6_newArr.push(result)
+    })
+    console.log("2. Es6_newArr : ["+Es6_newArr+"]")
+  }
+
+  render() {
+    return (
+      <h2>[ THIS IS ForEach ]</h2>
+    )
+  }
+}
+
+export default R014_ForEach;
