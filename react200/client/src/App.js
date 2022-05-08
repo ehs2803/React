4f92f273cfc49ab54689b048e19a7f44a9714311
ehs2803 <hr2803@naver.com>
2022-05-08 23:33:04 +0900

    20220508

--- react200/client/src/App.js
+++ react200/client/src/App.js
@@ -1,11 +1,24 @@
 import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import ImportConponent from "./R003_ImportConponent"
+import LifecycleEx from "./R007_LifecycleEx"
+import Es6 from "./R009_Es6"
+import Variable from './R010_Variable'
+import SpreadOperator from './R011_SpreadOperator'
 
 function App() {
   return (
     <div>
-	<h1>Start React 200!</h1>
-	<p>HTML 적용하기</p>
+	    <h1>Start React 200!</h1>
+	    <p>HTML 적용하기</p>
+        <ImportConponent></ImportConponent>
+        <LifecycleEx
+            prop_value = 'FromApp.js'
+        />
+        <Es6/>
+        <Variable/>
+        <SpreadOperator/>
     </div>
   );
 }
