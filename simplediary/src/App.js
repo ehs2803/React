bdc9173d944479a8a92899493eb7eff5cd83b547
ehs2803 <hr2803@naver.com>
2022-06-08 15:23:20 +0900

    diary-20220608

--- simplediary/src/App.js
+++ simplediary/src/App.js
@@ -1,7 +1,8 @@
-import { useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import './App.css';
 import DiaryEditor from './DiaryEditor';
 import DiaryList from './DiaryList';
+import LifeCycle from './LifeCycle';
 /*
 const dummyList = [
   {
@@ -33,6 +34,30 @@ function App() {
 
   const dataId = useRef(0);
 
+  const getData = async () => {
+    const res = await fetch(
+      "https://jsonplaceholder.typicode.com/comments"
+    ).then((res) => res.json());
+
+    const initData = res.slice(0, 20).map((it) => {
+      return {
+        author: it.email,
+        content: it.body,
+        emotion: Math.floor(Math.random() * 5) + 1,
+        created_date: new Date().getTime() + 1,
+        id: dataId.current++
+      };
+    });
+
+    setData(initData);
+  };
+
+  useEffect(() => {
+    setTimeout(() => {
+      getData();
+    }, 1500);
+  }, []);
+
   const onCreate = (author, content, emotion) => {
     const created_date = new Date().getTime();
     const newItem = {
@@ -46,10 +71,25 @@ function App() {
     setData([newItem, ...data]);
   };
 
+  const onRemove = (targetId) => {
+    console.log(`${targetId}가 삭제되었습니다`);
+    const newDiaryList = data.filter((it) => it.id !== targetId);
+    setData(newDiaryList);
+  };
+
+  const onEdit = (targetId, newContent) => {
+    setData(
+      data.map((it) =>
+        it.id === targetId ? { ...it, content: newContent } : it
+      )
+    );
+  };
+
   return (
     <div className="App">
+      <LifeCycle/>
       <DiaryEditor onCreate={onCreate}/>
-      <DiaryList diaryList={data}/>
+      <DiaryList onEdit={onEdit} onRemove={onRemove} diaryList={data}/>
     </div>
   );
 };
--- simplediary/src/DiaryItem.js
+++ simplediary/src/DiaryItem.js
@@ -1,4 +1,35 @@
-const DiaryItem = ({ id, author, content, emotion, created_date }) => {
+import { useRef, useState } from "react";
+
+const DiaryItem = ({ onRemove, onEdit, id, author, content, emotion, created_date }) => {
+
+  const localContentInput = useRef();
+  const [localContent, setLocalContent] = useState(content);
+  const [isEdit, setIsEdit] = useState(false);
+  const toggleIsEdit = () => setIsEdit(!isEdit);
+
+  const handleClickRemove = () => {
+    if (window.confirm(`${id}번째 일기를 정말 삭제하시겠습니까?`)) {
+      onRemove(id);
+    }
+  };
+
+  const handleQuitEdit = () => {
+    setIsEdit(false);
+    setLocalContent(content);
+  };
+
+  const handleEdit = () => {
+    if (localContent.length < 5) {
+      localContentInput.current.focus();
+      return;
+    }
+
+    if (window.confirm(`${id}번 째 일기를 수정하시겠습니까?`)) {
+      onEdit(id, localContent);
+      toggleIsEdit();
+    }
+  };
+
      return (
        <div className="DiaryItem">
          <div className="info">
@@ -8,7 +39,28 @@ const DiaryItem = ({ id, author, content, emotion, created_date }) => {
            <br />
            <span className="date">{new Date(created_date).toLocaleString()}</span>
          </div>
-         <div className="content">{content}</div>
+         <div className="content">
+        {isEdit ? (
+          <textarea
+            ref={localContentInput}
+            value={localContent}
+            onChange={(e) => setLocalContent(e.target.value)}
+          />
+        ) : (
+          content
+        )}
+      </div>
+          {isEdit ? (
+        <>
+          <button onClick={handleQuitEdit}>수정 취소</button>
+          <button onClick={handleEdit}>수정 완료</button>
+        </>
+      ) : (
+        <>
+          <button onClick={handleClickRemove}>삭제하기</button>
+          <button onClick={toggleIsEdit}>수정하기</button>
+        </>
+      )}
        </div>
      );
    };
